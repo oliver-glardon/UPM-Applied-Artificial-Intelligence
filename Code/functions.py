4802c41b38af3c5f27fc3067831c3dccd9c6df0f
ge25faz <ge25faz@mytum.de>
2020-05-11 12:01:10 +0200

    Oli: New Code from Oli

--- Code/functions.py
+++ Code/functions.py
@@ -1,8 +1,44 @@
+# Group Information: Intelligencia Artificial Applicada, UPM
+# .....
+# ....
+# Oliver Glardon, 19936
+#_______________________________________________________________________________________________________________________
+# imports
+from scipy.io import loadmat
+import os
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import numpy as np
 
+# Parameters for printing outputs
+line_str = "-------------"
 
 def display_number(data):
     plt.imsave('filename.png', np.array(data).reshape(28, 28), cmap=cm.gray)
     plt.imshow(np.array(data).reshape(28, 28))
+
+# Method to split data into training and testing data
+def split_into_train_and_test(data, percentage_of_training_data = 0.8):
+    print("Split into training and testing data\n" + line_str)
+    df_train_set = data.sample(frac=percentage_of_training_data, random_state=0)
+    df_test_set = data.drop(df_train_set.index)
+    df_train_input = pd.DataFrame(df_train_set.loc[:, df_train_set.columns != 'class'])
+    df_train_output = pd.DataFrame(df_train_set['class'])
+    df_test_input = pd.DataFrame(df_test_set.loc[:, df_test_set.columns != 'class'])
+    df_test_output = pd.DataFrame(df_test_set['class'])
+    return df_train_input, df_train_output, df_test_input, df_test_output
+
+def standardize(df_train_input, df_test_input):
+    print("Standardizing the data\n" + line_str)
+    stsc = StandardScaler()
+    stsc.fit(df_train_input)
+    df_train_input_ = pd.DataFrame(stsc.transform(df_train_input))
+    df_test_input_ = pd.DataFrame(stsc.transform(df_test_input))
+    return df_train_input, df_test_input
--- Code/main.py
+++ Code/main.py
@@ -10,7 +10,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import pandas as pd
 
 #_______________________________________________________________________________________________________________________
@@ -41,6 +44,8 @@ print("Input Dataframe"
 print(df_total)
 print(line_str)
 
+#_______________________________________________________________________________________________________________________
+print("Split into training and testing data\n" + line_str)
 # Split into training and testing data
 percentage_of_training_data = 0.8
 df_train_set = df_total.sample(frac=percentage_of_training_data, random_state=0)
@@ -72,7 +77,7 @@ df_train_input_ = pd.DataFrame(stsc.transform(df_train_input))
 df_test_input_ = pd.DataFrame(stsc.transform(df_test_input))
 
 # PCA
-minimum_explained_variance = 0.85
+minimum_explained_variance = 0.95
 pca = PCA(minimum_explained_variance)
 
 # alternative:
@@ -80,7 +85,7 @@ pca = PCA(minimum_explained_variance)
 # number_principal_components = 10
 # pca = PCA(n_components=number_principal_components)
 
-pca.fit(df_train_input)
+pca.fit(df_train_input_)
 principal_components_train = pca.transform(df_train_input_)
 number_principal_components = pca.n_components_
 MSE_PCA_train = 1 - pca.explained_variance_ratio_.sum()
@@ -88,9 +93,23 @@ pca_output = "Principal Component Analysis (PCA) \n\n" \
              "Explained Variance: " + str(round(pca.explained_variance_ratio_.sum(), 3))+ "" \
                 " \nNumber of Principal Components: " + str(number_principal_components) + "\n" + "" \
                 "MSE (training data): " + str(round(MSE_PCA_train, 3)) + "\n" + line_str
+print(pca_output)
 
 # calculate the PCs for the test data as well
 principal_components_test = pca.transform(df_test_input_)
+
+#_______________________________________________________________________________________________________________________
+# TASK 6: Fisher Discriminant
+fisher = LinearDiscriminantAnalysis()
+fisher.fit(df_train_input_, df_train_output['class'].values)
+fisher_components_train = fisher.transform(df_train_input_)
+fisher_components_test = fisher.transform(df_test_input_)
+
+# Adding Fisher Discrimant Analysis to PCA
+fisher_pca = LinearDiscriminantAnalysis()
+fisher_pca.fit(principal_components_train, df_train_output['class'].values)
+fisher_pca_components_train = fisher_pca.transform(principal_components_train)
+fisher_pca_components_test = fisher_pca.transform(principal_components_test)
 
 #_______________________________________________________________________________________________________________________
 # Text from homework description:
@@ -99,14 +118,53 @@ principal_components_test = pca.transform(df_test_input_)
 #_______________________________________________________________________________________________________________________
 # TASK 2: k-NN
 # ....
+print("k-nearest neighbors")
+num_neighbors = 3
+knn = KNeighborsClassifier(n_neighbors=num_neighbors)
+knn.fit(principal_components_train, df_train_output['class'].values)
+knn_labels_pred = knn.predict(principal_components_test)
+knn_results = (df_test_output['class'].values == knn_labels_pred)
+knn_percentage_of_correct_predictions = knn_results.sum()/len(knn_results)
+print("Dimensionality reduction: PCA" +
+      "\nNumber of nearest neighbors: "+ str(num_neighbors) +
+      "\nPercentage of correct prediction: "+ str(knn_percentage_of_correct_predictions) +
+      "\n" + line_str)
+
+print("knn with PCA and Fisher")
+knn.fit(fisher_pca_components_train, df_train_output['class'].values)
+knn_labels_pred = knn.predict(fisher_pca_components_test)
+knn_results = (df_test_output['class'].values == knn_labels_pred)
+knn_percentage_of_correct_predictions = knn_results.sum()/len(knn_results)
+print("Dimensionality reduction: PCA and Fisher (LDA)" +
+      "\nNumber of nearest neighbors: "+ str(num_neighbors) +
+      "\nPercentage of correct prediction: "+ str(knn_percentage_of_correct_predictions) +
+      "\n" + line_str)
 
 #_______________________________________________________________________________________________________________________
 # TASK 3: Bayesian Classifiers
 # ....
+print("Naive Bayes Classification")
+bayes = GaussianNB()
+bayes.fit(principal_components_train, df_train_output['class'].values)
+bayes_labels_pred = bayes.predict(principal_components_test)
+bayes_results = (df_test_output['class'].values == bayes_labels_pred)
+bayes_percentage_of_correct_predictions = bayes_results.sum()/len(bayes_results)
+print("\nMethod: Gaussian Naive Bayes" +
+      "\nPercentage of correct prediction: "+ str(bayes_percentage_of_correct_predictions) +
+      "\n" + line_str)
 
 #_______________________________________________________________________________________________________________________
 # TASK 4: MLP
 # ....
+print("Multilayer Perceptron (MLP)")
+mlp = MLPClassifier()
+mlp.fit(principal_components_train, df_train_output['class'].values)
+mlp_labels_pred = mlp.predict(principal_components_test)
+mlp_results = (df_test_output['class'].values == mlp_labels_pred)
+mlp_percentage_of_correct_predictions = mlp_results.sum()/len(mlp_results)
+print("\nMethod: STILL TO ADD" +
+      "\nPercentage of correct prediction: "+ str(mlp_percentage_of_correct_predictions) +
+      "\n" + line_str)
 
 #_______________________________________________________________________________________________________________________
 # TASK 5: SOM
@@ -114,7 +172,7 @@ principal_components_test = pca.transform(df_test_input_)
 
 #_______________________________________________________________________________________________________________________
 # Optional tasks for obtaining top marks above 7: (WE SHOULD ALSO DO THAT)
-# Task 6.- Use Linear Fischer Discriminant for reducing the dimensionality
+# Task 6.- Use Linear Fisher Discriminant for reducing the dimensionality
 # Task 7.- Use K-means for clustering the images
 # Task 8.- Use Autoencoder for dimensionality reduction (first reduce with PCA to an intermediate reduction level).
 # Compare the results with task 1.
